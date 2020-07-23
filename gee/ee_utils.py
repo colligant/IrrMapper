dcb4228c83751b2b1c5e8ac91efbea18e9d500e3
Thomas Colligan <thomas.colligan@umontana.edu>
2020-07-23 11:33:03 -0600

    Refactor extraction of data, move functions to utils file

--- gee/ee_utils.py
+++ gee/ee_utils.py
@@ -1,5 +1,58 @@
 import ee
+import geopandas as gpd
 import os
+from glob import glob
+from collections import defaultdict
+from pprint import pprint
+
+from shapefile_spec import shape_to_year_and_count as SHP_TO_YEAR_AND_COUNT
+
+YEARS = [2003, 2008, 2009, 2010, 2011, 2012, 2013, 2015]
+LC8_BANDS = ['B2',   'B3',    'B4',  'B5',  'B6',    'B7']
+LC7_BANDS = ['B1',   'B2',    'B3',  'B4',  'B5',    'B7']
+LC5_BANDS = ['B1',   'B2',    'B3',  'B4',  'B5',    'B7']
+STD_NAMES = ['blue', 'green', 'red', 'nir', 'swir1', 'swir2']
+
+def temporally_filter_features(shapefiles, year):
+    shapefile_to_feature_collection = {}
+    for shapefile in shapefiles:
+        is_temporal = True
+        bs = os.path.basename(shapefile)
+        feature_collection = ee.FeatureCollection(shapefile)
+        if 'irrigated' in bs and 'unirrigated' not in bs:
+            feature_collection = feature_collection.filter(ee.Filter.eq("YEAR", year))
+        elif 'fallow' in bs:
+            feature_collection = feature_collection.filter(ee.Filter.eq("YEAR", year))
+        else:
+            # don't need to temporally filter non-temporal land cover classes
+            is_temporal = False
+            shapefile_to_feature_collection[shapefile] = feature_collection
+            
+        if is_temporal:
+            valid_years = list(dict(SHP_TO_YEAR_AND_COUNT[bs].items()).keys())
+            if year in valid_years:
+                shapefile_to_feature_collection[shapefile] = feature_collection
+
+    return shapefile_to_feature_collection
+
+
+def create_class_labels(shapefile_to_feature_collection):
+    class_labels = ee.Image(0).byte()
+    for shapefile, feature_collection in shapefile_to_feature_collection.items():
+        class_labels = class_labels.paint(feature_collection, 
+                assign_class_code(shapefile)+1)
+    return class_labels.updateMask(class_labels)
+
+
+def preprocess_data(year):
+
+    l8 = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR').select(LC8_BANDS, STD_NAMES)
+    l7 = ee.ImageCollection('LANDSAT/LE07/C01/T1_SR').select(LC7_BANDS, STD_NAMES)
+    l5 = ee.ImageCollection('LANDSAT/LT05/C01/T1_SR').select(LC7_BANDS, STD_NAMES)
+    l7l8 = ee.ImageCollection(l7.merge(l8).merge(l5))
+
+    return temporalCollection(l7l8, ee.Date('{}-05-01'.format(year)), 6, 32, 'days')
+
 
 def temporalCollection(collection, start, count, interval, units):
  
@@ -13,6 +66,7 @@ def temporalCollection(collection, start, count, interval, units):
         ee.Number(interval).multiply(ee.Number(i).add(1)), units)
     return collection.filterDate(startDate, endDate).reduce(ee.Reducer.mean())
   return ee.ImageCollection(sequence.map(filt))
+
 
 def assign_class_code(shapefile_path):
   shapefile_path = os.path.basename(shapefile_path)
@@ -32,8 +86,43 @@ def assign_class_code(shapefile_path):
   else:
       raise NameError('shapefile path {} isn\'t named in assign_class_code'.format(shapefile_path))
 
+
+def is_temporal(shapefile_path):
+    shapefile_path = os.path.basename(shapefile_path)
+    if 'fallow' in shapefile_path:
+        return True
+    elif 'irrigated' in shapefile_path and 'unirrigated' not in shapefile_path:
+        return True
+    else:
+        return False
+
+
+def make_shape_to_year_and_count_dict(shapefile_dir):
+    '''
+    Uses local copies of shapefiles that have been
+    uploaded to GEE.
+    '''
+    shape_to_year_and_count = defaultdict(dict)
+
+    for f in glob(os.path.join(shapefile_dir, "*shp")):
+        gdf = gpd.read_file(f)
+        stripped_filename = os.path.splitext(os.path.basename(f))[0]
+        for year in YEARS:
+            if is_temporal(f):
+                shape_to_year_and_count[stripped_filename][year] = sum(gdf['YEAR'] == year)
+            else:
+                shape_to_year_and_count[stripped_filename][year] = gdf.shape[0]
+
+    return shape_to_year_and_count
+
 if __name__ == '__main__':
-    pass
+    shapefile_train = '/home/thomas/irrigated-training-data/ee-dataset/train/'
+    shapefile_test = '/home/thomas/irrigated-training-data/ee-dataset/test/'
+    shapefile_valid = '/home/thomas/irrigated-training-data/ee-dataset/validation/'
 
+    train = make_shape_to_year_and_count_dict(shapefile_train)
+    test = make_shape_to_year_and_count_dict(shapefile_test)
+    valid = make_shape_to_year_and_count_dict(shapefile_valid)
+    merged = {**train, **test, **valid} # wow python
 
-
+    pprint(merged)
--- gee/extract_data.py
+++ gee/extract_data.py
@@ -7,63 +7,16 @@ import os
 import numpy as np
 from random import shuffle
 
-from ee_utils import temporalCollection, assign_class_code
+from ee_utils import assign_class_code, preprocess_data, create_class_labels, temporally_filter_features
 from shapefile_spec import shape_to_year_and_count as SHP_TO_YEAR_AND_COUNT
 
 
-LC8_BANDS = ['B2',   'B3',    'B4',  'B5',  'B6',    'B7']
-LC7_BANDS = ['B1',   'B2',    'B3',  'B4',  'B5',    'B7']
-LC5_BANDS = ['B1',   'B2',    'B3',  'B4',  'B5',    'B7']
-STD_NAMES = ['blue', 'green', 'red', 'nir', 'swir1', 'swir2']
-
-KERNEL_SIZE = 512
+KERNEL_SIZE = 256
 KERNEL_SHAPE = [KERNEL_SIZE, KERNEL_SIZE]
 list_ = ee.List.repeat(1, KERNEL_SIZE)
 lists = ee.List.repeat(list_, KERNEL_SIZE)
 KERNEL = ee.Kernel.fixed(KERNEL_SIZE, KERNEL_SIZE, lists)
 GS_BUCKET = 'ee-irrigation-mapping'
-
-
-def preprocess_data(year):
-
-    l8 = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR').select(LC8_BANDS, STD_NAMES)
-    l7 = ee.ImageCollection('LANDSAT/LE07/C01/T1_SR').select(LC7_BANDS, STD_NAMES)
-    l5 = ee.ImageCollection('LANDSAT/LT05/C01/T1_SR').select(LC7_BANDS, STD_NAMES)
-    l7l8 = ee.ImageCollection(l7.merge(l8).merge(l5))
-
-    return temporalCollection(l7l8, ee.Date('{}-05-01'.format(year)), 6, 32, 'days')
-
-
-def temporally_filter_features(shapefiles, year):
-    shapefile_to_feature_collection = {}
-    for shapefile in shapefiles:
-        is_temporal = True
-        bs = os.path.basename(shapefile)
-        feature_collection = ee.FeatureCollection(shapefile)
-        if 'irrigated' in bs and 'unirrigated' not in bs:
-            feature_collection = feature_collection.filter(ee.Filter.eq("YEAR", year))
-        elif 'fallow' in bs:
-            feature_collection = feature_collection.filter(ee.Filter.eq("YEAR", year))
-        else:
-            # don't need to temporally filter non-temporal land cover classes
-            is_temporal = False
-            shapefile_to_feature_collection[shapefile] = feature_collection
-            
-        if is_temporal:
-            valid_years = list(dict(SHP_TO_YEAR_AND_COUNT[bs].items()).keys())
-            if year in valid_years:
-                shapefile_to_feature_collection[shapefile] = feature_collection
-
-    return shapefile_to_feature_collection
-
-
-def create_class_labels(shapefile_to_feature_collection):
-    class_labels = ee.Image(0).byte()
-    for shapefile, feature_collection in shapefile_to_feature_collection.items():
-        class_labels = class_labels.paint(feature_collection, 
-                assign_class_code(shapefile)+1)
-    return class_labels.updateMask(class_labels)
-
 
 def extract_data_over_shapefiles(mask_shapefiles, year,
         out_folder, points_to_extract=None, n_shards=10):
@@ -93,20 +46,18 @@ def extract_data_over_shapefiles(mask_shapefiles, year,
             # Frustratingly using getInfo() for sizes causes
             # a EE out of user memory error, because it has to actually
             # request something from the server
-            try:
-                n_features = SHP_TO_YEAR_AND_COUNT[os.path.basename(shapefile)][year]
-            except KeyError:
-                n_features = SHP_TO_COUNT[os.path.basename(shapefile)]
-            print(year, n_features, shapefile)
-            continue
+            n_features = SHP_TO_YEAR_AND_COUNT[os.path.basename(shapefile)][year]
             out_class_label = os.path.basename(shapefile)
             out_filename = out_class_label + "_" + str(year)
             geometry_sample = ee.ImageCollection([])
-            indices = np.arange(400)
-            print(out_class_label, year)
-            n = 30
-            indices = np.random.choice(indices, size=n)
+            if not n_features:
+                continue
+            n = 400
+            if n_features < n:
+                n = n_features
+            indices = np.random.choice(n_features, size=n)
             indices = [int(i) for i in indices] 
+            print(out_class_label, year, n_features, len(indices))
             feature_count = 0
             for i, idx in enumerate(indices):
                 sample = data_stack.sample(
@@ -208,9 +159,9 @@ if __name__ == '__main__':
     validation_pts = 'users/tcolligan0/points_to_extract/validation_regions_points'
 
     years = [2003, 2008, 2009, 2010, 2011, 2012, 2013, 2015]
-    extract_test = False
+    extract_test = True
     extract_train = True
-    extract_validation = False
+    extract_validation = True
     if extract_test:
         for year in years:
             extract_data_over_shapefiles(test, year,
@@ -218,8 +169,8 @@ if __name__ == '__main__':
     if extract_train:
         for year in years:
             extract_data_over_shapefiles(train, year, 
-                    out_folder='train-data-july23') #, points_to_extract=train_pts)
+                    out_folder='train-data-july23/') #, points_to_extract=train_pts)
     if extract_validation:
         for year in years:
             extract_data_over_shapefiles(validation, year, 
-                    out_folder='validation-data-july23', points_to_extract=validation_pts)
+                    out_folder='validation-data-july23/', points_to_extract=validation_pts)
