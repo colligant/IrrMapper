Fix NDVI calculation, change assign_class_code

gee/ai/trainer/confusion_matrix.py
@@ -1,7 +1,14 @@
 import tensorflow as tf
 
 from argparse import ArgumentParser
-from .utils import confusion_matrix_from_generator, make_test_dataset, make_yearly_test_dataset
+from . import utils 
+
+def numpyfy(dct):
+
+    out = {}
+    for k, v in dct.items():
+        out[k] = v.numpy()
+    return out
 
 
 if __name__ == '__main__':
@@ -9,9 +16,10 @@ if __name__ == '__main__':
     ap = ArgumentParser()
     ap.add_argument('--data-directory')
     ap.add_argument('--model-directory')
-    ap.add_argument('--batch-size', default=64)
+    ap.add_argument('--batch-size', type=int, default=64)
     ap.add_argument('--job-dir', default=64)
     ap.add_argument('--yearly-stats', action='store_true')
+    ap.add_argument('--n-classes', type=int)
 
     args = ap.parse_args()
     model_path = args.model_directory
@@ -20,10 +28,11 @@ if __name__ == '__main__':
     loaded = tf.saved_model.load(model_path)
     model = loaded.signatures['serving_default']
     if args.yearly_stats:
-        dataset_dict = make_yearly_test_dataset(args.data_directory)
+        dataset_dict = utils.make_yearly_test_dataset(args.data_directory)
         for year, dataset in dataset_dict.items():
             print(year, type(dataset))
-            c, p, r, i = confusion_matrix_from_generator([dataset], batch_size=args.batch_size, model=model)
+            c, p, r, i = utils.confusion_matrix_from_generator([dataset],
+                    batch_size=args.batch_size, model=model, n_classes=args.n_classes)
             print(c)
             print('------------')
             print(p)
@@ -34,16 +43,17 @@ if __name__ == '__main__':
             print('------------')
 
     else:
-        dataset = make_test_dataset(args.data_directory)
+        dataset = utils.make_test_dataset(args.data_directory, add_ndvi=False)
         if not isinstance(dataset, list):
             dataset = [dataset]
-        c, p, r, i = confusion_matrix_from_generator(dataset, batch_size=args.batch_size, model=model)
+        c, p, r, i = utils.confusion_matrix_from_generator(dataset, 
+                batch_size=args.batch_size, model=model, n_classes=args.n_classes)
 
         print('model path:', model_path)
-        print(c)
+        print(c.numpy())
         print('------------')
-        print(p)
+        print(numpyfy(p))
         print('------------')
-        print(r)
+        print(numpyfy(r))
         print('------------')
         print(i)

gee/ai/trainer/models.py
@@ -8,7 +8,7 @@ from tensorflow.keras.activations import relu
 
 
 _epsilon = tf.convert_to_tensor(K.epsilon(), tf.float32)
-weight_decay_const = 0.1
+weight_decay_const = 0.01
 
 def ConvBlock(x, filters=64, padding='same'):
     x = Conv2D(filters=filters, kernel_size=3, strides=1, padding=padding,

gee/ai/trainer/utils.py
@@ -2,17 +2,19 @@ import numpy as np
 import time
 import os
 import tensorflow as tf
+import tensorflow
 from collections import defaultdict
 
-# from . import feature_spec
-# from . import config
-import matplotlib.pyplot as plt
-import feature_spec
-import config
+from . import feature_spec
+from . import config
+# import matplotlib.pyplot as plt
+# import feature_spec
+# import config
 
 features_dict = feature_spec.features_dict()
 BANDS = feature_spec.bands() # includes mask raster
 FEATURES = feature_spec.features() # only input features
+NDVI_INDICES = [(2, 3), (8, 9), (14, 15), (20, 21), (26, 27), (32, 33)]
 
 def tf_distance_map(mask):
     im_shape = mask.shape
@@ -28,14 +30,14 @@ def distance_map(mask):
     return mask
 
 def random_rotate_image(image):
-  image = rotate(image, np.random.uniform(-30, 30), reshape=False)
-  return image
+    image = rotate(image, np.random.uniform(-30, 30), reshape=False)
+    return image
 
 def tf_random_rotate_image(image):
-  im_shape = image.shape
-  [image,] = tf.py_function(random_rotate_image, [image], [tf.float32])
-  image.set_shape(im_shape)
-  return image
+    im_shape = image.shape
+    [image,] = tf.py_function(random_rotate_image, [image], [tf.float32])
+    image.set_shape(im_shape)
+    return image
 
 def one_hot(labels, n_classes):
     h, w = labels.shape
@@ -82,7 +84,7 @@ def mask_unlabeled_values(y_true, y_pred):
     return y_true, y_pred
 
 
-def confusion_matrix_from_generator(datasets, batch_size, model, n_classes=3):
+def confusion_matrix_from_generator(datasets, batch_size, model, n_classes):
     ''' 
     inputs: list of tf.data.Datasets, not batched, without repeat.
     '''   
@@ -90,13 +92,12 @@ def confusion_matrix_from_generator(datasets, batch_size, model, n_classes=3):
     labels = range(n_classes)
     instance_count = 0
     for dataset in datasets:
-        dataset = dataset.batch(batch_size)
         for batch in dataset:
             features, y_true = batch[0], batch[1]
-            y_pred = model(features)['logits']
+            y_pred = model(features)['logits'].numpy()
             instance_count += y_pred.shape[0]
             y_true, y_pred = mask_unlabeled_values(y_true, y_pred)
-            cmat = tf.math.confusion_matrix(y_true, y_pred, num_classes=3)
+            cmat = tf.math.confusion_matrix(y_true, y_pred, num_classes=n_classes)
             out_cmat += cmat
     precision_dict = {}
     recall_dict = {}
@@ -108,7 +109,8 @@ def confusion_matrix_from_generator(datasets, batch_size, model, n_classes=3):
         recall_dict[i] = out_cmat[i, i] / np.sum(out_cmat[:, i]) # column i
     return out_cmat, recall_dict, precision_dict, instance_count
 
-def get_dataset(pattern):
+
+def get_dataset(pattern, add_ndvi):
   """Function to read, parse and format to tuple a set of input tfrecord files.
   Get all the files matching the pattern, parse and convert to tuple.
   Args:
@@ -122,8 +124,8 @@ def get_dataset(pattern):
   dataset = tf.data.TFRecordDataset(pattern, compression_type='GZIP',
           num_parallel_reads=8)
   dataset = dataset.map(parse_tfrecord, num_parallel_calls=5)
-  dataset = dataset.map(to_tuple, num_parallel_calls=5)
-
+  to_tup = to_tuple(add_ndvi)
+  dataset = dataset.map(to_tup, num_parallel_calls=5)
   return dataset
 
 def parse_tfrecord(example_proto):
@@ -136,7 +138,7 @@ def parse_tfrecord(example_proto):
   """
   return tf.io.parse_single_example(example_proto, features_dict)
 
-def to_tuple(inputs):
+def to_tuple(add_ndvi):
   """Function to convert a dictionary of tensors to a tuple of (inputs, outputs).
   Turn the tensors returned by parse_tfrecord into a stack in HWC shape.
   Args:
@@ -144,16 +146,23 @@ def to_tuple(inputs):
   Returns:
     A tuple of (inputs, outputs).
   """
-  features_list = [inputs.get(key) for key in sorted(FEATURES)]
-  stacked = tf.stack(features_list, axis=0)
-  # Convert from CHW to HWC
-  stacked = tf.transpose(stacked, [1, 2, 0])
-  image_stack = add_ndvi(stacked)
-  labels = one_hot(inputs.get('constant'), n_classes=5)
-  labels = tf.cast(labels, tf.int32)
-  return image_stack, labels
+  def to_tup(inputs):
+      features_list = [inputs.get(key) for key in sorted(FEATURES)]
+      stacked = tf.stack(features_list, axis=0)
+      # Convert from CHW to HWC
+      stacked = tf.transpose(stacked, [1, 2, 0]) * 0.0001
+      if add_ndvi:
+          image_stack = add_ndvi_raster(stacked)
+      else:
+          image_stack = stacked
+      # 'constant' is the label for label raster. 
+      labels = one_hot(inputs.get('constant'), n_classes=5)
+      labels = tf.cast(labels, tf.int32)
+      return image_stack, labels
 
-def add_ndvi(image_stack):
+  return to_tup
+
+def add_ndvi_raster(image_stack):
 
     '''
     These indices are hardcoded, and taken from the
@@ -172,11 +181,16 @@ def add_ndvi(image_stack):
         32 5_nir_mean
         33 5_red_mean
     '''
-    indices = [(2, 3), (8, 9), (14, 15), (20, 21), (26, 27), (32, 33)]
     out = []
-    for nir_idx, red_idx in indices:
+    for nir_idx, red_idx in NDVI_INDICES:
+        # Add a small constant in the denominator to ensure
+        # NaNs don't occur because of missing data. Missing
+        # data (i.e. Landsat 7 scan line failure) is represented as 0
+        # in TFRecord files. Adding \{epsilon} will barely 
+        # change the non-missing data, and will make sure missing data
+        # is still 0 when it's fed into the model.
         ndvi = (image_stack[:,:, nir_idx] - image_stack[:,:, red_idx]) /\
-                (image_stack[:,:, nir_idx] + image_stack[:,:, red_idx])
+                (image_stack[:,:, nir_idx] + image_stack[:,:, red_idx] + 1e-8) 
         out.append(ndvi)
     return tf.concat((image_stack, tf.stack(out, axis=-1)), axis=-1)
 
@@ -192,14 +206,14 @@ def filter_list_into_classes(lst):
 
     return out
 
-def make_training_dataset(root, batch_size=16):
+def make_training_dataset(root, add_ndvi, batch_size=16):
     pattern = "*gz"
     files = tf.io.gfile.glob(os.path.join(root, pattern))
     dataset = get_dataset(files)
     dataset = dataset.batch(batch_size).repeat().shuffle(buffer_size=config.BUFFER_SIZE)
     return dataset
 
-def make_balanced_training_dataset(root, batch_size=16):
+def make_balanced_training_dataset(root, add_ndvi, batch_size=16):
     pattern = "*gz"
     datasets = []
     files = tf.io.gfile.glob(os.path.join(root, pattern))
@@ -224,14 +238,14 @@ def sort_files_into_years(files):
 
     return year_to_files
 
-def make_yearly_test_dataset(root, batch_size=16):
+def make_yearly_test_dataset(root, add_ndvi, batch_size=16):
     pattern = "*gz"
     test_root = os.path.join(root, pattern)
     files = tf.io.gfile.glob(test_root)
     year_to_files = sort_files_into_years(files)
     year_to_dataset = {}
     for year, files in year_to_files.items():
-        year_to_dataset[year] = get_dataset(files)
+        year_to_dataset[year] = get_dataset(files, add_ndvi)
     return year_to_dataset
 
 good_years = set([2003, 2008, 2009, 2010, 2011, 2012, 2013, 2015])
@@ -247,35 +261,45 @@ def filter_unirrigated_years(files):
     return out
 
 
-def make_test_dataset(root, batch_size=16):
+def make_test_dataset(root, add_ndvi, batch_size=16):
     pattern = "*gz"
     training_root = os.path.join(root, pattern)
     files = tf.io.gfile.glob(training_root)
-    files = filter_unirrigated_years(files)
-    datasets = get_dataset(files).batch(config.BATCH_SIZE)
+    # files = filter_unirrigated_years(files)
+    datasets = get_dataset(files, add_ndvi).batch(config.BATCH_SIZE)
     return datasets
 
-def md(root, batch_size=16):
+def md(root, add_ndvi, batch_size=16):
     pattern = "*gz"
     training_root = os.path.join(root, pattern)
     files = tf.io.gfile.glob(training_root)
-    datasets = get_dataset(files).batch(1)
+    datasets = get_dataset(files, add_ndvi).batch(1)
     return datasets
 
 
 if __name__ == '__main__':
-
-    dataset = md('/tmp/')
-    for i, d in dataset:
-        d = d.numpy()
-        i = i.numpy()
-
-        mask = np.sum(d, axis=-1) == 0
-        d = np.argmax(d, axis=-1).astype(np.float32)
-        d[mask] = np.nan
-        for j in range(35, 42):
-            fig, ax = plt.subplots(ncols=2)
-            ax[0].imshow(i[0,:, :, j])
-            ax[1].imshow(d.squeeze())
-            plt.suptitle(j)
-            plt.show()
+    dataset = make_test_dataset('/tmp/', True)
+    '''
+    for j, (features, labels) in enumerate(dataset):
+        labels = labels.numpy()
+        features = features.numpy()
+        print(np.max(features))
+        if len(features[np.isnan(features)]):
+            for i in range(features.shape[-1]):
+                slc = features[0, :, :, i]
+                if len(slc[np.isnan(slc)]):
+                    fig, ax = plt.subplots(ncols=3)
+                    ax[0].imshow(features[0, :, :, 32])
+                    ax[1].imshow(features[0, :, :, 33])
+                    ax[2].imshow(features[0, :, :, 41])
+                    plt.show()
+    mask = np.sum(d, axis=-1) == 0
+    d = np.argmax(d, axis=-1).astype(np.float32)
+    d[mask] = np.nan
+    for j in range(35, 42):
+        fig, ax = plt.subplots(ncols=2)
+        ax[0].imshow(i[0,:, :, j])
+        ax[1].imshow(d.squeeze())
+        plt.suptitle(j)
+        plt.show()
+    '''