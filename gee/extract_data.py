202187490b9e2dac47458d46cf3f8044c7e47aad
Thomas Colligan <thomas.colligan@umontana.edu>
2020-08-14 09:58:29 -0600

    Refactor data extraction code into a class

--- gee/extract_data.py
+++ gee/extract_data.py
@@ -1,5 +1,5 @@
 import ee
-#ee.Authenticate()
+# ee.Authenticate()
 ee.Initialize()
 import tensorflow as tf
 import time
@@ -10,210 +10,163 @@ from random import shuffle
 import ee_utils
 from shapefile_spec import shape_to_year_and_count as SHP_TO_YEAR_AND_COUNT
 
-
-KERNEL_SIZE = 256
+KERNEL_SIZE = 64
 KERNEL_SHAPE = [KERNEL_SIZE, KERNEL_SIZE]
 list_ = ee.List.repeat(1, KERNEL_SIZE)
 lists = ee.List.repeat(list_, KERNEL_SIZE)
 KERNEL = ee.Kernel.fixed(KERNEL_SIZE, KERNEL_SIZE, lists)
 GS_BUCKET = 'ee-irrigation-mapping'
 
-def extract_test_patches(mask_shapefiles, year, 
-        out_folder, patch_shapefile):
+class GEEExtractor:
 
-    image_stack = ee_utils.preprocess_data(year).toBands()
-    # Features dict for TFRecord
-    features = [feat['id'] for feat in image_stack.getInfo()['bands']]
-    # Add in the mask raster
-    features = features + ['constant']
-    columns = [tf.io.FixedLenFeature(shape=KERNEL_SHAPE, dtype=tf.float32) for k in features]
-    feature_dict = dict(zip(features, columns))
+    def __init__(self, year, out_folder, mask_shapefiles, n_shards=10):
 
-    shapefile_to_feature_collection = ee_utils.temporally_filter_features(mask_shapefiles, year)
-    class_labels = ee_utils.create_class_labels(shapefile_to_feature_collection)
-    data_stack = ee.Image.cat([image_stack, class_labels]).float()
+        self.year = year
+        self.out_folder = out_folder
+        self.n_shards = n_shards
+        self.mask_shapefiles = mask_shapefiles
 
-    patches = ee.FeatureCollection(patch_shapefile)
-    patches = patches.toList(patches.size())
+        self._construct_data_stack()
 
-    out_filename = str(year)
-    for idx in range(patches.size().getInfo()):
+    
+    def _construct_data_stack(self):
+        image_stack = ee_utils.preprocess_data(self.year).toBands()
+        features = [feat['id'] for feat in image_stack.getInfo()['bands']]
+        self.features = features + ['constant']
+        self.shapefile_to_feature_collection = \
+                ee_utils.temporally_filter_features(self.mask_shapefiles, self.year)
+        class_labels = ee_utils.create_class_labels(self.shapefile_to_feature_collection)
+        self.image_stack = ee.Image.cat([image_stack, class_labels]).float()
+        self.data_stack = self.image_stack.neighborhoodToArray(KERNEL)
 
-        patch = ee.Feature(patches.get(idx))
-        features = [feat['id'] for feat in class_labels.getInfo()['bands']]
-        task = ee.batch.Export.image.toCloudStorage(
-                image=data_stack,
-                bucket=GS_BUCKET,
-                description=out_filename + str(time.time()),
-                fileNamePrefix=out_folder +  out_filename + str(time.time()),
-                fileFormat='TFRecord',
-                region=patch.geometry(),
-                scale=30,
-                formatOptions={'patchDimensions':256,
-                               'compressed':True},
-                )
-        task.start()
-
-
-
-def extract_data_over_shapefiles(mask_shapefiles, year,
-        out_folder, points_to_extract=None, n_shards=10):
-
-    image_stack = ee_utils.preprocess_data(year).toBands()
-    # Features dict for TFRecord
-    features = [feat['id'] for feat in image_stack.getInfo()['bands']]
-    # Add in the mask raster
-    features = features + ['constant']
-    columns = [tf.io.FixedLenFeature(shape=KERNEL_SHAPE, dtype=tf.float32) for k in features]
-    feature_dict = dict(zip(features, columns))
-
-    shapefile_to_feature_collection = ee_utils.temporally_filter_features(mask_shapefiles, year)
-    if points_to_extract is not None:
-        shapefile_to_feature_collection['points'] = points_to_extract
-
-    class_labels = ee_utils.create_class_labels(shapefile_to_feature_collection)
-    data_stack = ee.Image.cat([image_stack, class_labels]).float()
-    data_stack = data_stack.neighborhoodToArray(KERNEL)
-
-    if points_to_extract is None:
-        # This extracts data over every polygon that
-        # was passed in 
-        for shapefile, feature_collection in shapefile_to_feature_collection.items():
-
-            polygons = feature_collection.toList(feature_collection.size())
-            # Frustratingly using getInfo() for sizes causes
-            # a EE out of user memory error, because it has to actually
-            # request something from the server
-            n_features = SHP_TO_YEAR_AND_COUNT[os.path.basename(shapefile)][year]
-            out_class_label = os.path.basename(shapefile)
-            out_filename = out_class_label + "_" + str(year)
+    def extract_data_over_centroids(mask_shapefiles):
+        for shapefile, feature_collection in self.shapefile_to_feature_collection.items():
+            centroids = feature_collection.map(lambda feature: \
+                    ee.Feature(feature.geometry().centroid()))
+            centroids = centroids.toList(centroids.size())
+            n_features = SHP_TO_YEAR_AND_COUNT[os.path.basename(shapefile)][self.year]
+            out_filename = self._create_filename(shapefile)
             geometry_sample = ee.ImageCollection([])
-            if not n_features:
-                continue
-            n = 10
-            if n_features < n:
-                n = n_features
-            indices = np.random.choice(n_features, size=n)
-            indices = [int(i) for i in indices] 
-            print(out_class_label, year, n_features, len(indices))
+            indices = [int(i) for i in np.random.choice(n_features, size=n)]
             feature_count = 0
-            for i, idx in enumerate(indices):
+            for idx in indices:
                 sample = data_stack.sample(
+                        region=ee.Feature(centroids.get(idx)).geometry(),
+                        scale=30,
+                        numPixels=1,
+                        tileScale=8
+                        )
+                geometry_sample = geometry_sample.merge(sample)
+                if (feature_count+1) % n_shards == 0:
+                    geometry_sample = self._create_and_start_table_task(geometry_sample)
+                feature_count += 1
+            # take care of leftovers
+            self._create_and_start_table_task(geometry_sample)
+
+
+    def extract_data_over_patches(self, patch_shapefile):
+        if isinstance(patch_shapefile, list):
+            for p_shp in patch_shapefile:
+                p_shp = ee.FeatureCollection(p_shp)
+                patches = p_shp.toList(centroids.size())
+                out_filename = self._create_filename(patch_shapefile)
+                for idx in range(patches.size().getInfo()):
+                    patch = ee.Feature(patches.get(idx))
+                    self._create_and_start_image_task(patch, out_filename)
+        else:
+            patches = ee.FeatureCollection(patch_shapefile)
+            patches = patches.toList(patches.size())
+            out_filename = self._create_filename(patch_shapefile)
+            for idx in range(patches.size().getInfo()):
+                patch = ee.Feature(patches.get(idx))
+                self._create_and_start_image_task(patch, out_filename)
+                break
+
+
+    def extract_data_over_shapefiles(self):
+        for shapefile, feature_collection in self.shapefile_to_feature_collection.items():
+            polygons = polygons.toList(centroids.size())
+            n_features = SHP_TO_YEAR_AND_COUNT[os.path.basename(shapefile)][self.year]
+            out_filename = self._create_filename(shapefile)
+            print(out_filename)
+            geometry_sample = ee.ImageCollection([])
+            indices = [int(i) for i in np.random.choice(n_features, size=n)]
+            feature_count = 0
+            for idx in indices:
+                sample = self.data_stack.sample(
                         region=ee.Feature(polygons.get(idx)).geometry(),
                         scale=30,
                         numPixels=1,
                         tileScale=8
                         )
                 geometry_sample = geometry_sample.merge(sample)
-                feature_count += 1
                 if (feature_count+1) % n_shards == 0:
-                    task = ee.batch.Export.table.toCloudStorage(
-                            collection=geometry_sample,
-                            description=out_filename + str(time.time()),
-                            bucket=GS_BUCKET,
-                            fileNamePrefix=out_folder + out_filename + str(time.time()),
-                            fileFormat='TFRecord',
-                            selectors=features
-                            )
-                    try:
-                        task.start()
-                    except ee.ee_exception.EEException:
-                        print('waiting to export, sleeping for 50 minutes. Holding at\
-                                {} {}, index {}'.format(year, shapefile, i))
-                        time.sleep(3000)
-                        task.start()
-                    geometry_sample = ee.ImageCollection([])
+                    geometry_sample = self._create_and_start_table_task(geometry_sample,
+                            out_filename)
+                feature_count += 1
             # take care of leftovers
-            print('n_extracted:', feature_count)
-            task = ee.batch.Export.table.toCloudStorage(
-                    collection=geometry_sample,
-                    description=out_filename + str(time.time()),
-                    bucket=GS_BUCKET,
-                    fileNamePrefix=out_folder + out_filename + str(time.time()),
-                    fileFormat='TFRecord',
-                    selectors=features
-                    )
-            task.start()
-    else:
-        # just extract data at points
-        polygons = ee.FeatureCollection(points_to_extract)
-        polygons = polygons.toList(polygons.size())
-        n_features = polygons.size().getInfo() # see if this works
-        print(n_features, points_to_extract)
-        geometry_sample = ee.ImageCollection([])
-        out_filename = str(year)
-        n_extracted = 0
-        for i in range(n_features):
-            sample = data_stack.sample(
-                    region=ee.Feature(polygons.get(i)).geometry(),
-                    scale=30,
-                    numPixels=1,
-                    tileScale=8
-                    )
-            geometry_sample = geometry_sample.merge(sample)
-            if (i+1) % n_shards == 0:
-                n_extracted += geometry_sample.size().getInfo()
-                task = ee.batch.Export.table.toCloudStorage(
-                        collection=geometry_sample,
-                        bucket=GS_BUCKET,
-                        description=out_filename + str(time.time()),
-                        fileNamePrefix=out_folder + out_filename + str(time.time()),
-                        fileFormat='TFRecord',
-                        selectors=features
-                        )
-                task.start()
-                geometry_sample = ee.ImageCollection([])
-        # take care of leftovers
-        task = ee.batch.Export.table.toCloudStorage(
-                collection=geometry_sample,
+            self._create_and_start_table_task(geometry_sample)
+
+    def _create_and_start_image_task(self, patch, out_filename):
+        task = ee.batch.Export.image.toCloudStorage(
+                image=self.image_stack,
                 bucket=GS_BUCKET,
                 description=out_filename + str(time.time()),
-                fileNamePrefix=out_folder + out_filename + str(time.time()),
+                fileNamePrefix=self.out_folder +  out_filename + str(time.time()),
+                fileFormat='TFRecord',
+                region=patch.geometry(),
+                scale=30,
+                formatOptions={'patchDimensions':256,
+                               'compressed':True},
+                )
+        self._start_task_and_handle_exception(task)
+    
+    def _create_and_start_table_task(self, geometry_sample, out_filename):
+        task = ee.batch.Export.table.toCloudStorage(
+                collection=geometry_sample,
+                description=out_filename + str(time.time()),
+                bucket=GS_BUCKET,
+                fileNamePrefix=self.out_folder + out_filename + str(time.time()),
                 fileFormat='TFRecord',
                 selectors=features
                 )
-        task.start()
-        print(n_extracted, year)
+        geom = self._start_task_and_handle_exception(task)
+        return geom
+
+    def _start_task_and_handle_exception(self, task):
+
+        try:
+            task.start()
+        except ee.ee_exception.EEException:
+            print('waiting to export, sleeping for 50 minutes')
+            time.sleep(3000)
+            task.start()
+        return ee.ImageCollection([])
+
+    def _create_filename(self, shapefile):
+
+        return os.path.basename(shapefile) + str(self.year)
+
+
+''' steps:
+
+1. Make grid from extent.
+2. Split grid into test/train.
+3. Clip mask shapefiles to grid.
+4. Get intersections of grid shapefiles and mask shapefiles
+5. Upload to earth-engine.
+6. Extract data.
+
+'''
+
 
 if __name__ == '__main__':
 
-    test_root = 'users/tcolligan0/test/'
-    test = ['fallow_test', 'irrigated_test', 'uncultivated_test', 
-            'unirrigated_test', 'wetlands_test']
-    test = [test_root + t for t in test]
+    root = 'users/tcolligan0/train-aug12/'
+    train_shapefiles = ['irrigated_train', 'uncultivated_train', 'unirrigated_train',
+            'wetlands_train', 'fallow_train']
+    train_shapefiles = [root + s for s in train_shapefiles]
 
-    train = ['fallow_train', 'irrigated_train', 'uncultivated_train',
-            'unirrigated_train', 'wetlands_train']
-    train_root = 'users/tcolligan0/train/'
-    train = [train_root + t for t in train]
-
-    validation = ['fallow_validation', 'irrigated_validation', 'uncultivated_validation',
-            'unirrigated_validation', 'wetlands_validation']
-    validation_root = 'users/tcolligan0/validation/'
-    validation = [validation_root + t for t in validation]
-
-    test_pts = 'users/tcolligan0/points_to_extract/test_regions_points'
-    train_pts = 'users/tcolligan0/points_to_extract/train_regions_points'
-    validation_pts = 'users/tcolligan0/points_to_extract/validation_regions_points'
-
-    test_patches = 'users/tcolligan0/buffered_test_valid_patches/test_regions_buffered'
-    validation_patches = 'users/tcolligan0/buffered_test_valid_patches/validation_regions_buffered'
-
-    years = [2003, 2008, 2009, 2010, 2011, 2012, 2013, 2015]
-    # years = [2015]
-    extract_test = False
-    extract_train = False
-    extract_validation = True
-    if extract_test:
-        for year in years:
-            extract_test_patches(test, year,
-                    out_folder='test-patches-july28/', patch_shapefile=test_patches)
-    if extract_train:
-        for year in years:
-            extract_data_over_shapefiles(train, year, 
-                    out_folder='vae_data/l8cloudmask2015/') #, points_to_extract=train_pts)
-    if extract_validation:
-        for year in years:
-            extract_test_patches(validation, year, 
-                    out_folder='validation-patches-for-real-july28/',
-                    patch_shapefile=validation_patches)
+    patches = 'users/tcolligan0/test_block_aug14'
+    extractor = GEEExtractor(2013, 'aaa256/', train_shapefiles)
+    extractor.extract_data_over_patches(patches)
