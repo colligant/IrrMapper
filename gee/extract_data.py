517b3996138552aa1cf6bbb5bb5ab335ba5a2d36
Thomas Colligan <thomas.colligan@umontana.edu>
2020-08-17 12:52:48 -0600

    Refactor extraction code into class

--- gee/extract_data.py
+++ gee/extract_data.py
@@ -10,21 +10,18 @@ from random import shuffle
 import ee_utils
 from shapefile_spec import shape_to_year_and_count as SHP_TO_YEAR_AND_COUNT
 
-KERNEL_SIZE = 64
-KERNEL_SHAPE = [KERNEL_SIZE, KERNEL_SIZE]
-list_ = ee.List.repeat(1, KERNEL_SIZE)
-lists = ee.List.repeat(list_, KERNEL_SIZE)
-KERNEL = ee.Kernel.fixed(KERNEL_SIZE, KERNEL_SIZE, lists)
-GS_BUCKET = 'ee-irrigation-mapping'
 
 class GEEExtractor:
 
-    def __init__(self, year, out_folder, mask_shapefiles, n_shards=10):
+    def __init__(self, year, out_gs_bucket, out_folder, mask_shapefiles, kernel_size=256,
+            n_shards=10):
 
         self.year = year
         self.out_folder = out_folder
+        self.out_gs_bucket = out_gs_bucket
         self.n_shards = n_shards
         self.mask_shapefiles = mask_shapefiles
+        self.kernel_size = kernel_size
 
         self._construct_data_stack()
 
@@ -37,7 +34,11 @@ class GEEExtractor:
                 ee_utils.temporally_filter_features(self.mask_shapefiles, self.year)
         class_labels = ee_utils.create_class_labels(self.shapefile_to_feature_collection)
         self.image_stack = ee.Image.cat([image_stack, class_labels]).float()
-        self.data_stack = self.image_stack.neighborhoodToArray(KERNEL)
+        list_ = ee.list.repeat(1, self.kernel_size)
+        lists = ee.list.repeat(list_, self.kernel_size)
+        kernel = ee.kernel.fixed(kernel_size, kernel_size, lists)
+        self.data_stack = self.image_stack.neighborhoodToArray(kernel)
+
 
     def extract_data_over_centroids(mask_shapefiles):
         for shapefile, feature_collection in self.shapefile_to_feature_collection.items():
@@ -65,10 +66,13 @@ class GEEExtractor:
 
 
     def extract_data_over_patches(self, patch_shapefiles, buffer_region=None):
+        '''
+        Extracts TFRecords over a ROI 
+        '''
         if isinstance(patch_shapefiles, list):
             for patch_shapefile in patch_shapefiles:
                 patch_shapefile = ee.FeatureCollection(patch_shapefile)
-                patches = patch_shapefile.toList(centroids.size())
+                patches = patch_shapefile.toList(patch_shapefile.size())
                 out_filename = self._create_filename(patch_shapefiles)
                 for idx in range(patches.size().getInfo()):
                     patch = ee.Feature(patches.get(idx))
@@ -83,8 +87,8 @@ class GEEExtractor:
                 patch = ee.Feature(patches.get(idx))
                 if buffer_region is not None:
                     patch = patch.buffer(buffer_region)
+                    patch = ee.Feature(patch.bounds())
                 self._create_and_start_image_task(patch, out_filename)
-                break
 
 
     def extract_data_over_shapefiles(self):
@@ -122,7 +126,8 @@ class GEEExtractor:
                 region=patch.geometry(),
                 scale=30,
                 formatOptions={'patchDimensions':256,
-                               'compressed':True},
+                               'compressed':True,
+                               'maskedThreshold':0.99},
                 )
         self._start_task_and_handle_exception(task)
     
@@ -142,7 +147,8 @@ class GEEExtractor:
 
         try:
             task.start()
-        except ee.ee_exception.EEException:
+        except ee.ee_exception.EEException as e:
+            print(e)
             print('waiting to export, sleeping for 50 minutes')
             time.sleep(3000)
             task.start()
@@ -162,17 +168,45 @@ class GEEExtractor:
 5. Upload to earth-engine.
 6. Extract data.
 
+8/17: Extract irr. data at centroids, uncultivated/wetlands/unirrigated 
+over tiles.
 '''
 
 
 if __name__ == '__main__':
 
-    root = 'users/tcolligan0/train-aug12/'
+    gs_bucket = 'ee-irrigation-mapping'
+    test_root = 'users/tcolligan0/test-data-aug17/'
+    test_shapefiles = ['irrigated_test', 'uncultivated_test', 'unirrigated_test',
+            'wetlands_test', 'fallow_test']
+    test_shapefiles = [test_root + s for s in test_shapefiles]
+
+    train_root = 'users/tcolligan0/train-data-aug17/'
     train_shapefiles = ['irrigated_train', 'uncultivated_train', 'unirrigated_train',
             'wetlands_train', 'fallow_train']
-    train_shapefiles = [root + s for s in train_shapefiles]
+    train_shapefiles = [train_root + s for s in train_shapefiles]
 
-    patches = 'users/tcolligan0/test_block_aug14'
-    train_shapefiles = ['users/tcolligan0/data_test_block']
-    extractor = GEEExtractor(2013, 'aug14buffered2/', train_shapefiles)
-    extractor.extract_data_over_patches(patches, buffer_region=6000)
+    train_patches = ['irrigated_train_regions', 'uncultivated_train_regions', 
+                     'uncultivated_train_regions']
+
+    years = [2008, 2015]
+    patches = 'users/tcolligan0/test-data-aug17/test_regions'
+    extract_test = False
+    extract_train = True
+    if extract_test:
+        for year in years:
+            extractor = GEEExtractor(year, 
+                                     out_gs_bucket=out_gs_bucket, 
+                                     out_folder='test-data-aug17/', 
+                                     mask_shapefiles=test_shapefiles)
+
+            extractor.extract_data_over_patches(patches, buffer_region=2500)
+    if extract_train:
+        for year in years:
+            for patches in train_patches:
+                extractor = GEEExtractor(year, 
+                                         out_gs_bucket=out_gs_bucket, 
+                                         out_folder='train-data-aug17/', 
+                                         mask_shapefiles=train_shapefiles)
+
+                extractor.extract_data_over_patches(patches, buffer_region=2500)
